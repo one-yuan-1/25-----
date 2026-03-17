c2bd62c641b14f5c950a064c13f6fc1b502ceb34
one-yuan-1 <cba740417070508@163.com>
2026-03-17 19:17:06 +0800

    更改

---
+++ vue_frontend/src/components/view_select.vue
@@ -0,0 +1,26 @@
+<template>
+
+        <div class = "select">
+        <div>
+            <div>请输入您想查看的用户</div>
+            <form>
+                <input
+                placeholder="请输入您想查看的用户名"
+                />
+
+            </form>
+            <button>提交</button>
+
+        </div>
+
+    </div>
+
+
+</template>
+
+<script setup>
+</script>
+
+<style>
+@import "./view_select.css";
+</style>
